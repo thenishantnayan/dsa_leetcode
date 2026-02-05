602a22ff9b0161575e003e81d34f8387b9257fb0
Nishant Nayan <165375931+thenishantnayan@users.noreply.github.com>
2026-02-05 22:54:08 +0530

    Time: 0 ms (100%), Space: 16.5 MB (46.4%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -9,22 +9,50 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
 
-    TreeNode* bst(vector<int>&preorder,int &index,int lower,int upper)
+    /*
+        This function constructs the BST using preorder traversal.
+        
+        preorder -> given preorder array
+        index    -> current index in preorder (passed by reference so it updates globally)
+        lower    -> lower bound for valid BST values
+        upper    -> upper bound for valid BST values
+    */
+    TreeNode* bst(vector<int>& preorder, int &index, int lower, int upper)
     {
-        if(index == preorder.size() || lower>preorder[index] || upper <preorder[index])
+        // Base case 1:
+        // If all elements are used, stop recursion
+        if (index == preorder.size())
             return nullptr;
 
-        TreeNode* root = new TreeNode(preorder[index++]);
-        root->left = bst(preorder,index,lower,root->val);
-        root->right = bst(preorder,index,root->val,upper);
+        // Base case 2:
+        // If current value does not lie within valid BST range,
+        // it cannot be part of this subtree
+        if (preorder[index] < lower || preorder[index] > upper)
+            return nullptr;
 
+        // Create the root node using current preorder value
+        TreeNode* root = new TreeNode(preorder[index]);
+
+        // Move to next element in preorder
+        index++;
+
+        // All values for left subtree must be:
+        // lower < value < root->val
+        root->left = bst(preorder, index, lower, root->val);
+
+        // All values for right subtree must be:
+        // root->val < value < upper
+        root->right = bst(preorder, index, root->val, upper);
+
+        // Return constructed subtree
         return root;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        int index= 0;
-        return bst(preorder,index,INT_MIN,INT_MAX);
+        int index = 0;
+        return bst(preorder, index, INT_MIN, INT_MAX);
     }
 };
