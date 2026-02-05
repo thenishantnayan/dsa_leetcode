b095b347a919187b69da2086dcea6182765e1b65
thenishantnayan <nayannishant999@gmail.com>
2026-02-05 19:00:35 +0530

    Add LeetCode 230 - Kth Smallest Element in a BST

---
+++ BST/kth_smallest_element.cpp
@@ -0,0 +1,20 @@
+//leetcode 230. Kth Smallest Element in a BST
+
+class Solution {
+public:
+
+    void inorder(TreeNode* root,vector<int>&ans)
+    {
+        if(!root)
+        return;
+
+        inorder(root->left,ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int>ans;
+        inorder(root,ans);
+        return ans[k-1];
+    }
+};
