7a8626d9393a93485458c06c93aaa2d73075b03c
Nishant Nayan <165375931+thenishantnayan@users.noreply.github.com>
2026-02-26 02:00:36 +0530

    Time: 4 ms (79.03%), Space: 128.4 MB (56.01%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -3,19 +3,19 @@ public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int n = nums.size();
         vector<int>ans(n);
-        int pos =0,neg=1;
+        int i=0,j=1;
         //ans is assigned with the size so push back is not possible , assign values to the index
-        for(int i=0;i<n;i++)
+        for(int k=0;k<n;k++)
         {
-            if(nums[i]>0)
+            if(nums[k]>0)
             {
-                ans[pos]=nums[i];
-                pos+=2;
+                ans[i]=nums[k];
+                i+=2;
             }
             else
             {
-                ans[neg]=nums[i];
-                neg+=2;
+                ans[j]=nums[k];
+                j+=2;
             }
         }
         return ans;
