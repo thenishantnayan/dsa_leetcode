cd2a11df07633b1a9a0f75127186d26043bfc231
Nishant Nayan <165375931+thenishantnayan@users.noreply.github.com>
2026-02-28 20:38:40 +0530

    Time: 17 ms (37.61%), Space: 65.8 MB (39.36%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -11,33 +11,35 @@
  */
 class Solution {
 public:
-
+    // mid value nikalo and root node banao and use inorder traversal for that     nodes 
     void inorder(TreeNode* root,vector<int>&ans)
     {
         if(!root)
-            return;
+        return;
 
         inorder(root->left,ans);
         ans.push_back(root->val);
         inorder(root->right,ans);
     }
+
     TreeNode* bst(vector<int>&ans,int start,int end)
     {
         if(start>end)
         return nullptr;
 
-        int mid = start +(end-start)/2;
+        int mid = (start+end)/2;
+
         TreeNode* root = new TreeNode(ans[mid]);
-        root->left = bst(ans,start,mid-1);
-        root->right = bst(ans,mid+1,end);
-            
+
+        root->left= bst(ans,start,mid-1);
+        root->right= bst(ans,mid+1,end);
+
         return root;
-            
     }
     TreeNode* balanceBST(TreeNode* root) {
         vector<int>ans;
         inorder(root,ans);
 
-        return bst(ans,0,ans.size()-1);         
+        return bst(ans,0,ans.size()-1);
     }
 };
