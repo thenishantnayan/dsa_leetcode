520ded06ed7f65d2bb532be15919ddc5d27596ce
Nishant Nayan <165375931+thenishantnayan@users.noreply.github.com>
2026-03-02 01:20:06 +0530

    Time: 2 ms (46.01%), Space: 16.4 MB (97.24%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -2,5 +2,8 @@ class Solution {
 public:
     int minPartitions(string n) {
         return *max_element(n.begin(),n.end())-'0';
+
+        //for this return the maximum integer which needed deci binary numbers
+        // to convert single digit string into integer '7'-'0'
     }
 };
