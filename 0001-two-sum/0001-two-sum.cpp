9eaa274442bc9500aa382dee586506b418f268f8
Nishant Nayan <165375931+thenishantnayan@users.noreply.github.com>
2026-04-23 01:54:24 +0530

    Time: 0 ms (100%), Space: 14.7 MB (53.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++)
-        {
-            int rem = target - nums[i]; // remaining nikalo
-            if(mp.find(rem) != mp.end()) // find kro rem hai ya nhi , hai to 
-                return {mp[rem],i}; //return index dena hai  
-            
-            mp[nums[i]] = i; //store current value baad me check krne k liy
-        }
-        return {};
+       unordered_map<int,int>mp;
+       int n=nums.size();
+       
+       for(int i=0;i<n;i++)
+       {
+        int rem = target - nums[i];
+        if(mp.find(rem) != mp.end())
+            return {mp[rem],i};
+
+        mp[nums[i]]=i;
+       }
+       return {}; 
     }
 };
