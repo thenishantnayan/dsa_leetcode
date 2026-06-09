870f736fa8ef7a8daf9db7aae2a4f04b64c89a93
Nishant Nayan <165375931+thenishantnayan@users.noreply.github.com>
2026-06-10 00:53:43 +0530

    Time: 87 ms (33.48%), Space: 88.9 MB (49.94%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        int longest =0;
         unordered_set<int>st(nums.begin(),nums.end());
-        int longest = 0;
-
         for(int num : st){
-            if(st.find(num-1) == st.end())
-                {
-                    int currentnum = num;
-                    int cnt = 1;
-
-                while(st.find(currentnum +1) != st.end()){
-                    currentnum++;
-                    cnt++;
-                }
-                longest = max(longest,cnt);
-        }
+            if(st.find(num-1) == st.end()){ //check num-1 99 nhi hai to 
+                int currentnum = num; //current num 100
+                int count =1; // count 1 se hi 
+            
+            while(st.find(currentnum+1) != st.end()){ //uske aage ka present hai 
+                currentnum++; //new num ko aage badhao
+                count++; //count bhi 
+            }
+            longest = max(longest,count); // longest ko update kro 
+            }
         }
         return longest;
     }
