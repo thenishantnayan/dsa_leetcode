943ac134d1190bdfa177bbb3f798532932c5932b
Nishant Nayan <165375931+thenishantnayan@users.noreply.github.com>
2026-06-19 20:21:47 +0530

    Time: 0 ms (100%), Space: 12.6 MB (38.91%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n = nums.size();
-        int start = 0, end = n - 1;
-        // we keep shrinking the search space
-        // until start and end meet at peak index
-        while(start < end)
-        {
-            int mid = start + (end - start) / 2;
-            // compare current element with next element
-            // this tells us whether we are going UP or DOWN
-            if(nums[mid] < nums[mid + 1])
-            {
-                // slope is increasing (↗️)
-                // peak must be on RIGHT side
-                start = mid + 1;
-            }
+        int start = 0, end = nums.size()-1;
+        while(start<end){
+            int mid = start+(end-start)/2;
+            if(nums[mid]<nums[mid+1])
+                start = mid+1;
             else
-            {
-                // slope is decreasing (↘️)
-                // peak is on LEFT side or at mid
-                end = mid;
-            }
+                end =  mid;
         }
-        // when start == end, we found a peak index
         return start;
     }
 };
