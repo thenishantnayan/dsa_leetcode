b1364ae9d110a7b06cf5cca2bd94af884bd36141
Nishant Nayan <165375931+thenishantnayan@users.noreply.github.com>
2026-07-02 00:31:25 +0530

    Time: 3 ms (29.4%), Space: 8.6 MB (20.9%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,10 +5,9 @@ public:
         while(x!=0){
             rem = x%10;
 
-            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && rem > 7))
+            if (rev > INT_MAX / 10 || (rev <INT_MIN/10))
                 return 0;
-            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && rem < -8))
-                return 0;
+            
             rev = rev*10+rem;
             x = x/10;
         }
