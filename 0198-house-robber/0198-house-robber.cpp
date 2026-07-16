19b5756cbfef358b24a759775e7494bbd90a29ee
Nishant Nayan <165375931+thenishantnayan@users.noreply.github.com>
2026-07-16 13:49:03 +0530

    Time: 0 ms (100%), Space: 10.1 MB (89.89%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev1=0,prev2=0;
+
+        for(int money: nums){
+            int curr = max(prev1,prev2+money);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
