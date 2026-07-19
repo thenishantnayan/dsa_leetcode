61816ec7e4f7b462546a594d1eb1d6f3bf78cc3a
Nishant Nayan <165375931+thenishantnayan@users.noreply.github.com>
2026-07-19 22:25:57 +0530

    Time: 0 ms (100%), Space: 15 MB (12.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int need =0;
         unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            need = target - nums[i];
-            if(mp.find(need) != mp.end())
-                return {mp[need],i};
+        for(int i=0; i<nums.size();i++){
+            int need = target - nums[i];
 
-            mp[nums[i]] = i;
+            if(mp.find(need) != mp.end())
+                return {i,mp[need]};
+
+            mp[nums[i]] = i; //most mistske yaha index usko save kr rhe jisme mp ke nums ke i ko ith me store krenge 
         }
-        return{};
+        return {};
     }
 };
